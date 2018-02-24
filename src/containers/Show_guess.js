73937399cdeb56beeb6a5b3eb61cc2c838cd2d2b
toletole412 <yoonjioh90@gmail.com>
2018-02-24 23:31:26 +0100

    add guess input place

---
+++ src/containers/Show_guess.js
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react'
+import './Show_guess.css'
+
+class ShowGuess extends PureComponent {
+  render() {
+    return (
+      <div>
+
+        <ul className="ShowGuess">
+        //put input rendering 
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default ShowGuess
